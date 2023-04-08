1664bb9e80fd48ef1ae664cd3242320fb10432a3
Gaëtan <gaetan.coton@outlook.com>
2023-04-08 23:14:58 +0200

    Models, controllers, templates, routes for Type

--- catalogue/urls.py
+++ catalogue/urls.py
@@ -1,22 +1,15 @@
-"""reservations URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/2.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""reservations.catalogue URL Configuration
 """
-from django.contrib import admin
-from django.urls import include, path
+from django.urls import path
+
+from . import views
+
+app_name='catalogue'
 
 urlpatterns = [
-    path('catalogue/', include('catalogue.urls')),
-    path('admin/', admin.site.urls),
+    path('artist/', views.artist.index, name='artist_index'),
+    path('artist/<int:artist_id>', views.artist.show, name='artist_show'),
+    path('type/', views.type.index, name='type_index'),
+    path('type/<int:type_id>', views.type.show, name='type_show'),
 ]
+
